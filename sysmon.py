ff29d0d76b6016e1bf7a1a9ae2c9451eb9ea0e38
Sergey <xxsmotur@gmail.com>
2020-12-27 13:56:39 +0500

    return number of stat from docker

--- sysmon.py
+++ sysmon.py
@@ -3,3 +3,11 @@ import sh
 def free_mem():
     free = sh.grep(sh.free('-m'), 'Mem').split()[3]
     return f'Free memory: {free}'
+
+def docker():
+    c = lambda x: int(sh.wc(sh.docker(x), '-l'))-1
+    print(f'Running containers: {c("ps")}')
+    print(f'Total containers: {c("images")}')
+
+print(free_mem())
+docker()
